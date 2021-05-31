415bf1e1563b6ad3618ac56684be5c6fc99b5354
kyb <kyb>
2021-05-31 16:07:12 +0900

    kaizen

--- inception_v3.py
+++ inception_v3.py
@@ -1,17 +1,19 @@
 import numpy as np
-from keras.applications.inception_v3 import InceptionV3
-from keras.preprocessing import image
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+import time
 
 if __name__ == '__main__':
     model = InceptionV3(include_top=True, weights='imagenet')
 
-    img_path = 'elephant.jpeg'
+    img_path = 'Elephant.jpeg'
     img = image.load_img(img_path, target_size=(299, 299))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
 
     x = preprocess_input(x)
-
+    st= time.time()
     preds = model.predict(x)
+    print("inffffffffffffff: ",time.time()-st)
     print('Predicted:', decode_predictions(preds))
